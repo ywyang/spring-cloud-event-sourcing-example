02ed558c028b994e4842c472465b8d326884c307
yuewei.yang <yuewei.yang@outlook.com>
2018-09-01 11:54:26 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 set -e
 set -x
 
-export ONE_BOX=10.11.0.4
+export ONE_BOX=10.0.0.11
 export REGION=alauda01
 export SPACE_NAME=global
-export REGISTRY=10.11.0.6:5000
-export ALB_IP=10.11.0.6
-export GIT_HOST=10.11.0.5:9988
+export REGISTRY=10.0.0.6:5000
+export ALB_IP=10.0.0.6
+export GIT_HOST=10.0.0.6:9988
 export NGINX=nginx-10-11-0-6
 export AUTH_TOKEN=97898f42b8c95098df3f82037f43bf13fa33ff53
 
